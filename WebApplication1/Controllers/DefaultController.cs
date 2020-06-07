5e15345d6adc9a34de554608bdc3f27c70816da8
Allen <Yohoo0987@gmail.com>
2020-06-07 08:35:37 +0800

    LineSDKbot package

--- WebApplication1/Controllers/DefaultController.cs
+++ WebApplication1/Controllers/DefaultController.cs
@@ -78,7 +78,6 @@ namespace WebApplication1.Controllers
         {
             return Ok(await _products.SaveImageToBlobstroage(products));
         }
-        [HttpGet]
         public async Task<IActionResult> onMessageReply()
         {
             try
@@ -113,94 +112,94 @@ namespace WebApplication1.Controllers
             return Ok(my);
 
         }
-        //[HttpGet]
-        // public async Task<IActionResult> onMessageReply()
-        //{
-        //    //get configuration from appsettings.json
-        //    var token = "6km4N/DHLORGlvFVN2javPkUtIqzH/6XXeshLNjXHWbXdoafNas+ywY26FAcxpO9onJ+igrHae4L07FkU0IYOa8RjQUJ90OHhrbHXa2clltCUUbCW+9VFlVR62fqA4i6wLHHvsLR64Qgr7r5KAio1wdB04t89/1O/w1cDnyilFU=";// _config.GetSection("LINE-Bot-Setting:channelAccessToken");
-        //    var AdminUserId = _lineBotConfig.user_ID;// _config.GetSection("LINE-Bot-Setting:adminUserID");
-        //    var body = ""; //for JSON Body
-        //    //create vot instance
-        //    var bot = new isRock.LineBot.Bot(token);
-        //    isRock.LineBot.MessageBase responseMsg = null;
-        //    //message collection for response multi-message 
-        //    List<isRock.LineBot.MessageBase> responseMsgs =
-        //        new List<isRock.LineBot.MessageBase>();
+        [HttpGet]
+        public async Task<IActionResult> onMessagePushy()
+        {
+            //get configuration from appsettings.json
+            var token = "6km4N/DHLORGlvFVN2javPkUtIqzH/6XXeshLNjXHWbXdoafNas+ywY26FAcxpO9onJ+igrHae4L07FkU0IYOa8RjQUJ90OHhrbHXa2clltCUUbCW+9VFlVR62fqA4i6wLHHvsLR64Qgr7r5KAio1wdB04t89/1O/w1cDnyilFU=";// _config.GetSection("LINE-Bot-Setting:channelAccessToken");
+            var AdminUserId = _lineBotConfig.user_ID;// _config.GetSection("LINE-Bot-Setting:adminUserID");
+            var body = ""; //for JSON Body
+            //create vot instance
+            var bot = new isRock.LineBot.Bot(token);
+            isRock.LineBot.MessageBase responseMsg = null;
+            //message collection for response multi-message 
+            List<isRock.LineBot.MessageBase> responseMsgs =
+                new List<isRock.LineBot.MessageBase>();
 
-        //    try
-        //    {
+            try
+            {
 
-        //        //get JSON Body
-        //        using (StreamReader reader = new StreamReader(Request.Body, System.Text.Encoding.UTF8))
-        //        {
-        //            body = reader.ReadToEndAsync().Result;
-        //        }
-        //        //parsing JSON
-        //        var ReceivedMessage = isRock.LineBot.Utility.Parsing(body);
-        //        //Get LINE Event
-        //        var LineEvent = ReceivedMessage.events.FirstOrDefault();
-        //        //prepare reply message
-        //        if (LineEvent.type.ToLower() == "message")
-        //        {
-        //            switch (LineEvent.message.type.ToLower())
-        //            {
-        //                case "text":
-        //                    //add text response
-        //                    responseMsg =
-        //                        new isRock.LineBot.TextMessage($"you said : {LineEvent.message.text}");
-        //                    responseMsgs.Add(responseMsg);
-        //                    //add ButtonsTemplate if user say "/Show ButtonsTemplate"
-        //                    if (LineEvent.message.text.ToLower().Contains("/show buttonstemplate"))
-        //                    {
-        //                        //define actions
-        //                        var act1 = new isRock.LineBot.MessageAction()
-        //                        { text = "test action1", label = "test action1" };
-        //                        var act2 = new isRock.LineBot.MessageAction()
-        //                        { text = "test action2", label = "test action2" };
+                //get JSON Body
+                using (StreamReader reader = new StreamReader(Request.Body, System.Text.Encoding.UTF8))
+                {
+                    body = reader.ReadToEndAsync().Result;
+                }
+                //parsing JSON
+                var ReceivedMessage = isRock.LineBot.Utility.Parsing(body);
+                //Get LINE Event
+                var LineEvent = ReceivedMessage.events.FirstOrDefault();
+                //prepare reply message
+                if (LineEvent.type.ToLower() == "message")
+                {
+                    switch (LineEvent.message.type.ToLower())
+                    {
+                        case "text":
+                            //add text response
+                            responseMsg =
+                                new isRock.LineBot.TextMessage($"you said : {LineEvent.message.text}");
+                            responseMsgs.Add(responseMsg);
+                            //add ButtonsTemplate if user say "/Show ButtonsTemplate"
+                            if (LineEvent.message.text.ToLower().Contains("/show buttonstemplate"))
+                            {
+                                //define actions
+                                var act1 = new isRock.LineBot.MessageAction()
+                                { text = "test action1", label = "test action1" };
+                                var act2 = new isRock.LineBot.MessageAction()
+                                { text = "test action2", label = "test action2" };
 
-        //                        var tmp = new isRock.LineBot.ButtonsTemplate()
-        //                        {
-        //                            text = "Button Template text",
-        //                            title = "Button Template title",
-        //                            thumbnailImageUrl = new Uri("https://i.imgur.com/wVpGCoP.png"),
-        //                        };
+                                var tmp = new isRock.LineBot.ButtonsTemplate()
+                                {
+                                    text = "Button Template text",
+                                    title = "Button Template title",
+                                    thumbnailImageUrl = new Uri("https://i.imgur.com/wVpGCoP.png"),
+                                };
 
-        //                        tmp.actions.Add(act1);
-        //                        tmp.actions.Add(act2);
-        //                        //add TemplateMessage into responseMsgs
-        //                        responseMsgs.Add(new isRock.LineBot.TemplateMessage(tmp));
-        //                    }
-        //                    break;
-        //                case "sticker":
-        //                    responseMsg =
-        //                    new isRock.LineBot.StickerMessage(1, 2);
-        //                    responseMsgs.Add(responseMsg);
-        //                    break;
-        //                default:
-        //                    responseMsg = new isRock.LineBot.TextMessage($"None handled message type : { LineEvent.message.type}");
-        //                    responseMsgs.Add(responseMsg);
-        //                    break;
-        //            }
-        //        }
-        //        else
-        //        {
-        //            responseMsg = new isRock.LineBot.TextMessage($"None handled event type : { LineEvent.type}");
-        //            responseMsgs.Add(responseMsg);
-        //        }
+                                tmp.actions.Add(act1);
+                                tmp.actions.Add(act2);
+                                //add TemplateMessage into responseMsgs
+                                responseMsgs.Add(new isRock.LineBot.TemplateMessage(tmp));
+                            }
+                            break;
+                        case "sticker":
+                            responseMsg =
+                            new isRock.LineBot.StickerMessage(1, 2);
+                            responseMsgs.Add(responseMsg);
+                            break;
+                        default:
+                            responseMsg = new isRock.LineBot.TextMessage($"None handled message type : { LineEvent.message.type}");
+                            responseMsgs.Add(responseMsg);
+                            break;
+                    }
+                }
+                else
+                {
+                    responseMsg = new isRock.LineBot.TextMessage($"None handled event type : { LineEvent.type}");
+                    responseMsgs.Add(responseMsg);
+                }
 
-        //        //回覆訊息
-        //        bot.ReplyMessage(LineEvent.replyToken, responseMsgs);
-        //        //response OK
-        //        return Ok();
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        //如果有錯誤，push給admin
-        //        bot.PushMessage(AdminUserId, "Exception : \n" + ex.Message);
-        //        //response OK
-        //        return Ok();
-        //    }
-        //}
+                //回覆訊息
+                bot.ReplyMessage(LineEvent.replyToken, responseMsgs);
+                //response OK
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                //如果有錯誤，push給admin
+                bot.PushMessage(AdminUserId, "Exception : \n" + ex.Message);
+                //response OK
+                return Ok();
+            }
+        }
 
     }
 }
